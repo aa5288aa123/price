0b7f2bab7dcbdc9ec778103dbf5a689d79072fd3
郁勛 黃 <106111233@mail.oit.edu.tw>
2021-06-18 12:02:36 +0800

    改

--- price/drink.aspx
+++ price/drink.aspx
@@ -10,31 +10,7 @@
 <body>
     <form id="form1" runat="server">
         <div>
-                        <h2><asp:Label ID="Label1" runat="server" Text="菜單"></asp:Label></h2>
-            
 
-
-        
-        <asp:CheckBox ID="CheckBox1" runat="server" Text="珍珠奶茶"/>      
-        <asp:Label ID="Label2" runat="server" Text="45元"></asp:Label>
-        <asp:HiddenField id="A_text" runat="server" value="45元"/>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <asp:CheckBox ID="CheckBox2" runat="server" Text="紅茶"/>
-        <asp:Label ID="Labe3" runat="server" Text="20元"></asp:Label>
-        <asp:HiddenField id="B_text" runat="server" value="20元"/>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <asp:CheckBox ID="CheckBox3" runat="server" Text="冬瓜茶"/>
-        <asp:Label ID="Label4" runat="server" Text="25元"></asp:Label>
-        <asp:HiddenField id="C_text" runat="server" value="25元"/>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <asp:CheckBox ID="CheckBox4" runat="server" Text="奶茶"/>
-        <asp:Label ID="Label5" runat="server" Text="25元"></asp:Label>
-        <asp:HiddenField id="D_text" runat="server" value="25元"/>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <asp:CheckBox ID="CheckBox5" runat="server" Text="養樂多綠茶"/>
-        <asp:Label ID="Label6" runat="server" Text="30元"></asp:Label>
-        <asp:HiddenField id="E_text" runat="server" value="30元"/>
-        <asp:Button ID="Button1" runat="server" Text="送出" PostBackUrl="drink2.aspx"/>
         </div>
     </form>
 </body>
--- price/drink2.aspx.cs
+++ price/drink2.aspx.cs
@@ -11,46 +11,7 @@ namespace price
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (Request.Form.Get("CheckBox1") == "on")
-            {
-                {
-                    lb_Res.Text = lb_Res.Text + "珍珠奶茶" + "<br />";
-                    string s_text = Request.Form.Get("A_text");
-                    Response.Write(s_text);
-                }
-            }
-            if (Request.Form.Get("CheckBox2") == "on")
-            {
-                {
-                    lb_Res.Text = lb_Res.Text + "紅茶" + "<br />";
-                    string s_text = Request.Form.Get("B_text");
-                    Response.Write(s_text);
-                }
-            }
-            if (Request.Form.Get("CheckBox3") == "on")
-            {
-                {
-                    lb_Res.Text = lb_Res.Text + "冬瓜茶" + "<br />";
-                    string s_text = Request.Form.Get("C_text");
-                    Response.Write(s_text);
-                }
-            }
-            if (Request.Form.Get("CheckBox4") == "on")
-            {
-                {
-                    lb_Res.Text = lb_Res.Text + "奶茶" + "<br />";
-                    string s_text = Request.Form.Get("D_text");
-                    Response.Write(s_text);
-                }
-            }
-            if (Request.Form.Get("CheckBox5") == "on")
-            {
-                {
-                    lb_Res.Text = lb_Res.Text + "養樂多綠茶" + "<br />";
-                    string s_text = Request.Form.Get("E_text");
-                    Response.Write(s_text);
-                }
-            }
+
 
 
 
